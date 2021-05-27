171cb68c10616662d3277458b2538bfbad68745f
menna131 <menna_allah_helal@hotmail.com>
2021-05-27 16:01:03 +0200

    FIX: Dashboard

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['activePage' => 'profile', 'titlePage' => __('User Profile')])
+@extends('layouts.app', ['activePage' => 'user-management', 'titlePage' => __('User Profile')])
 
 @section('content')
     <div class="content">
